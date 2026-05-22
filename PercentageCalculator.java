ff00930f6ac70b4d56fbc802e2780f877a7c576c
Debanshi Mandal <mandaldebanshi15@gmail.com>
2026-05-22 13:53:07 +0530

    Create PercentageCalculator.java

---
+++ PercentageCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class PercentageCalculator {
+  public static void main (String []args) {
+    Scanner per = new Scanner (System.in)
+      System.out.println("Enter num: ");
+    float num = per.nextFloat();
+    float percentage = (num/100)*100;
+    System.out.println(Your perentage out of 100 is: ");
+    System.out.println(percentage);
+  }
+}
+    
